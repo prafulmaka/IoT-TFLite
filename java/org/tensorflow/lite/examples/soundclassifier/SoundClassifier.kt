7894c56d5a0e113fa55845a1d61f37a1d82c6e72
prafulmaka <57310445+prafulmaka@users.noreply.github.com>
2021-05-24 15:41:08 -0700

    Update SoundClassifier.kt

--- java/org/tensorflow/lite/examples/soundclassifier/SoundClassifier.kt
+++ java/org/tensorflow/lite/examples/soundclassifier/SoundClassifier.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2020 The TensorFlow Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package org.tensorflow.lite.examples.soundclassifier
 
@@ -43,8 +29,6 @@ import org.tensorflow.lite.support.common.FileUtil
 /**
  * Performs classification on sound.
  *
- * <p>The API supports models which accept sound input via {@code AudioRecord} and one classification output tensor.
- * The output of the recognition is emitted as LiveData of Map.
  *
  */
 class SoundClassifier(context: Context, private val options: Options = Options()) :
